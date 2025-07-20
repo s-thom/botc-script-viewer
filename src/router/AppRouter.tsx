dbf179736e1e92aca2552e14562156dcef1ed591
Stuart Thomson <me@sthom.kiwi>
2025-07-20 19:02:05 +1200

    Add some random scripts I've come across in my travels

--- src/router/AppRouter.tsx
+++ src/router/AppRouter.tsx
@@ -3,6 +3,7 @@ import { BrowserRouter, Navigate, Route, Routes } from "react-router";
 import { ScriptPageWrapper } from "../components/misc/ScriptPageWrapper";
 import { BASE_3 } from "../scripts/base3";
 import { CAROUSEL_COLLECTION } from "../scripts/carousel-collection";
+import { RANDOM_SCRIPTS } from "../scripts/random";
 import { WORLD_CUP_25 } from "../scripts/wc25";
 
 const BotCScriptsPage = lazy(() => import("../pages/BotCScriptsPage"));
@@ -14,6 +15,20 @@ const HostedScriptPage = lazy(() => import("../pages/HostedScriptPage"));
 const HomePage = lazy(() => import("../pages/Home"));
 const UrlScriptPage = lazy(() => import("../pages/UrlScriptPage"));
 
+const HOSTED_SCRIPTS = {
+  b3: BASE_3,
+  cc: CAROUSEL_COLLECTION,
+  wc25: WORLD_CUP_25,
+  r: RANDOM_SCRIPTS,
+};
+
+const DYNAMIC_ROUTES = {
+  "s/:id": BotCScriptsPage,
+  "u/:url": UrlScriptPage,
+  "gz/:json": CompressedJSONPage,
+  "uncompressed/:json": UncompressedJSONPage,
+};
+
 export function AppRouter() {
   return (
     <BrowserRouter>
@@ -22,77 +37,32 @@ export function AppRouter() {
         {/* <Route path="/error" element={<ErrorPage />} />
         <Route path="/appload" element={<AppLoadingPage />} />
         <Route path="/scriptload" element={<ScriptLoadingPage />} /> */}
-        <Route path="b3">
-          {BASE_3.map((script) => (
-            <Route
-              key={script.id}
-              path={script.id}
-              element={
-                <ScriptPageWrapper>
-                  <HostedScriptPage script={script} />
-                </ScriptPageWrapper>
-              }
-            />
-          ))}
-        </Route>
-        <Route path="cc">
-          {CAROUSEL_COLLECTION.map((script) => (
-            <Route
-              key={script.id}
-              path={script.id}
-              element={
-                <ScriptPageWrapper>
-                  <HostedScriptPage script={script} />
-                </ScriptPageWrapper>
-              }
-            />
-          ))}
-        </Route>
-        <Route path="wc25">
-          {WORLD_CUP_25.map((script) => (
-            <Route
-              key={script.id}
-              path={script.id}
-              element={
-                <ScriptPageWrapper>
-                  <HostedScriptPage script={script} />
-                </ScriptPageWrapper>
-              }
-            />
-          ))}
-        </Route>
-        <Route
-          path="s/:id"
-          element={
-            <ScriptPageWrapper>
-              <BotCScriptsPage />
-            </ScriptPageWrapper>
-          }
-        />
-        <Route
-          path="u/:url"
-          element={
-            <ScriptPageWrapper>
-              <UrlScriptPage />
-            </ScriptPageWrapper>
-          }
-        />
-        <Route
-          path="gz/:json"
-          element={
-            <ScriptPageWrapper>
-              <CompressedJSONPage />
-            </ScriptPageWrapper>
-          }
-        />
-        <Route
-          path="uncompressed/:json"
-          element={
-            <ScriptPageWrapper>
-              <UncompressedJSONPage />
-            </ScriptPageWrapper>
-          }
-        />
+        {Object.entries(HOSTED_SCRIPTS).map(([path, scripts]) => (
+          <Route key={path} path={path}>
+            {scripts.map((script) => (
+              <Route
+                key={script.id}
+                path={script.id}
+                element={
+                  <ScriptPageWrapper>
+                    <HostedScriptPage script={script} />
+                  </ScriptPageWrapper>
+                }
+              />
+            ))}
+          </Route>
+        ))}
+        {Object.entries(DYNAMIC_ROUTES).map(([path, Component]) => (
+          <Route
+            key={path}
+            path={path}
+            element={
+              <ScriptPageWrapper>
+                <Component />
+              </ScriptPageWrapper>
+            }
+          />
+        ))}
         <Route path="/*" element={<Navigate to="/" />} />
       </Routes>
     </BrowserRouter>
---
+++ src/scripts/random/README.md
@@ -0,0 +1,3 @@
+# Random Scripts
+
+These are various scripts I've collected on my travels. This list is not a "best of".
