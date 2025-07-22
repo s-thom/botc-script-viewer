b9ba7aac27f06ade684be94c6d6fdff84d3356a6
Stuart Thomson <me@sthom.kiwi>
2025-07-22 20:25:45 +1200

    Add error pages

--- src/types/site.ts
+++ src/types/site.ts
@@ -2,3 +2,26 @@ export interface PageMeta {
   title?: string;
   description?: string;
 }
+
+export interface AppErrorOptions {
+  title?: string;
+  description?: string;
+}
+
+export class AppError extends Error {
+  private readonly options: AppErrorOptions;
+
+  constructor(message: string, options: AppErrorOptions) {
+    super(message);
+
+    this.options = options;
+  }
+
+  get title() {
+    return this.options.title;
+  }
+
+  get description() {
+    return this.options.description;
+  }
+}
