7c56dd73a4e1e2899a9b5b7613e034d0e7f5efbf
Stuart Thomson <me@sthom.kiwi>
2025-07-25 21:31:57 +1200

    Add Node adapter for Astro

--- astro.config.mjs
+++ astro.config.mjs
@@ -4,11 +4,19 @@ import { defineConfig } from "astro/config";
 import react from "@astrojs/react";
 import { ALLOWED_EXTERNAL_HOSTNAMES } from "./src/lib/images";
 
+import node from "@astrojs/node";
+
 // https://astro.build/config
 export default defineConfig({
   integrations: [react()],
+
   image: {
     domains: ALLOWED_EXTERNAL_HOSTNAMES,
   },
+
   scopedStyleStrategy: "class",
+
+  adapter: node({
+    mode: "standalone",
+  }),
 });
