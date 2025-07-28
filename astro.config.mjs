9ae59b00b2e334344109fb389d26957c839b32b5
Stuart Thomson <me@sthom.kiwi>
2025-07-29 11:01:40 +1200

    Log env variable during build for testing

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,8 @@ import { ALLOWED_EXTERNAL_HOSTNAMES } from "./src/lib/images";
 
 const { SITE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
 
+console.log({ SITE_DOMAIN });
+
 // https://astro.build/config
 export default defineConfig({
   site: SITE_DOMAIN ? `https://${SITE_DOMAIN}` : undefined,
