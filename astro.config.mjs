310b805086c6b464747566f38c343cb16df895e4
Stuart Thomson <me@sthom.kiwi>
2025-07-29 11:06:32 +1200

    Try resolve environment variable issues

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,16 +1,14 @@
 // @ts-check
 import cloudflare from "@astrojs/cloudflare";
 import { defineConfig } from "astro/config";
-import { loadEnv } from "vite";
 import { ALLOWED_EXTERNAL_HOSTNAMES } from "./src/lib/images";
-
-const { SITE_DOMAIN } = loadEnv(process.env.NODE_ENV, process.cwd(), "");
-
-console.log({ SITE_DOMAIN });
 
 // https://astro.build/config
 export default defineConfig({
-  site: SITE_DOMAIN ? `https://${SITE_DOMAIN}` : undefined,
+  site:
+    process.env.ENVIRONMENT === "production"
+      ? `https://botc-script-viewer.sthom.kiwi`
+      : undefined,
   integrations: [],
   image: {
     domains: ALLOWED_EXTERNAL_HOSTNAMES,
