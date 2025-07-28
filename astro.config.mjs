82d406901c3c13bfd489b0e464ef59ded0029826
Stuart Thomson <me@sthom.kiwi>
2025-07-29 10:15:19 +1200

    Remove React Astro integration as it's no longer used

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,18 +1,14 @@
 // @ts-check
 import cloudflare from "@astrojs/cloudflare";
-import react from "@astrojs/react";
 import { defineConfig } from "astro/config";
 import { ALLOWED_EXTERNAL_HOSTNAMES } from "./src/lib/images";
 
 // https://astro.build/config
 export default defineConfig({
-  integrations: [react()],
-
+  integrations: [],
   image: {
     domains: ALLOWED_EXTERNAL_HOSTNAMES,
   },
-
   scopedStyleStrategy: "class",
-
   adapter: cloudflare({ imageService: "cloudflare" }),
 });
